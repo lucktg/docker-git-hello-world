14c2fc99aa6bdacfe019aaf0bd4a6c1da6205200
vtorres <vtorres@switchfly.com>
2020-05-15 21:15:11 -0500

    Changed file for auto build

--- src/main/java/org/sample/HelloWorldPing.java
+++ src/main/java/org/sample/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 
     public static void main(String args[]) throws Exception{
         for(int i = 0 ; i < 100; i++){
-            System.out.println("Hello World NEW Ping " + i );
+            System.out.println("Hello World NEW Ping other" + i );
             Thread.sleep(1000);
         }
     }
